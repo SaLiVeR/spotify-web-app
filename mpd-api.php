6840e7c16bda410cec5736e8688cf5f34df9d27a
Michael Fiford <michaelfiford@hotmail.com>
2013-01-20 17:32:14 +0000

    Change name and directory of mpd class

--- mpd-api.php
+++ mpd-api.php
@@ -10,7 +10,7 @@ require('config.php');
 //TODO: Set authentication key
 if(!isset($_GET['action'])) invalid();
 
-include('mpd.class.php');
+include(RESOURCE_DIR . 'class_mpd.php');
 $MPD = new MPD('localhost',6600);
 
 switch($_GET['action']) {
