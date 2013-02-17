21f7224073a7bfb59080bfa0abb36bfe8627cc40
Michael Fiford <michaelfiford@hotmail.com>
2013-02-17 18:50:48 +0000

    Think that's most of the php stuff done for this part of the file upload. Pass the failure info to javascript.

--- upload.php
+++ upload.php
@@ -25,9 +25,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $UploadInfo = $Upload->post(false);
         //Error check here        
-        foreach($UploadInfo['files'] as $File) {
+        foreach($UploadInfo['files'] as $FileKey => $File) {
             $PassedTests = true;
             $CurrentInfo = array();
+            $FailedTests = array();
             
             $FileInfo = $getID3->analyze("uploads/" . $File->name);
             $TagInfo = $FileInfo['tags']['id3v2'];
@@ -37,25 +38,30 @@ switch ($_SERVER['REQUEST_METHOD']) {
                     //If the missing information is "essential"
                     if(array_key_exists($RI, $RequiredInfo)) $PassedTests = false;
                     $CurrentInfo[$RI] = '';
+                    $FailedTests[] = $RI;
                 } else {
                     $CurrentInfo[$RI] = $TagInfo[$RI][0];
                 }
             }
             //Required data exists. Track moves on to admin approval.
-            $DB->query("INSERT INTO uploaded_songs (UploaderID, Filename, Title, Artist, Album, Year, Duration, Complete) VALUES (
+            echo "INSERT INTO uploaded_songs (UploaderID, Filename, Title, Artist, Album, Year, Duration, Complete) VALUES (
                 '" . $User->ID . "',
-                '" . db_string($File) . "',
+                '" . db_string($File->name) . "',
                 '" . db_string($CurrentInfo['title']) . "',
                 '" . db_string($CurrentInfo['artist']) . "',
                 '" . db_string($CurrentInfo['album']) . "',
                 '" . db_string($CurrentInfo['year']) . "',
                 '" . db_string(round($FileInfo['playtime_seconds'])) . "',
-                '" . ($PassedTests) ? 1 : 0 . "'
-            )");
-            $UploadInfo->Successful = $PassedTests;
+                '" . (($PassedTests) ? 1 : 0) . "'
+            )";
+            $UploadInfo['files'][$FileKey]->Successful = $PassedTests;
+            if(!$PassedTests) {
+                $UploadInfo['files'][$FileKey]->Failed = $FailedTests;
+            }
         }
-        
-        
+
+        echo json_encode($UploadInfo);
+
         break;
     case 'DELETE':
         $Upload->delete();
